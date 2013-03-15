ff9b8d54049aab5c243061a87a2eb68726068a20
Leigh Pauls <leigh.pauls@gmail.com>
2013-03-14 23:49:34 -0400

    2 sensor angle detection

--- ultra_reader/turn_control.ino
+++ ultra_reader/turn_control.ino
@@ -1,44 +1,40 @@
 #include "turn_control_state.h"
 
-#define DESIRED_DIST 0.25 // dist from the wall I want
-#define ACCEPTABLE_PATH_ERROR 0.05 // half the width of the acceptable path to be on
-#define ROC_IN_ERROR 0.3 // ROC of distance to turn to if out of path
+#define DESIRED_DIST 0.25 // meters - dist from the wall I want
+#define DEGREES_PER_METER  (15.0 / 0.1) // how hard to turn back to center
 
-#define ROC_KP 10.0// degress per m/s
+#define ANGLE_KP 1.0 // servo degress per boat direction degrees
 
-#define SPEED 1.0 // m/s
+#define SENSOR_SPREAD 0.1 // meters - distance between sensors
 
 void turn_control_init(TurnControlState *state) {
   
 }
 
-double turn_control_cycle(TurnControlState *state, double cur_dist, double cur_roc, double cur_time) {
-  double angle = atan(cur_roc/SPEED);
-  double x = cur_dist * cos(angle);
-  double x_roc = cur_roc * cos(angle);
+double turn_control_cycle(TurnControlState *state, double front_dist, double back_dist, double cur_time) {
+  double dist_diff = front_dist - back_dist;
+  if (abs(dist_diff) > SENSOR_SPREAD) {
+    dist_diff = SENSOR_SPREAD * dist_diff / abs(dist_diff);
+  }
+  double angle_deg = asin(dist_diff/SENSOR_SPREAD) * 180.0 / PI;
+  
+  double x = back_dist;
   
   double pos_error = x - DESIRED_DIST;
-  double desired_roc = 0.0;
-  
-  
-  if (abs(pos_error) > ACCEPTABLE_PATH_ERROR) {
-    if (pos_error > 0) {
-      desired_roc = -ROC_IN_ERROR;
-    } else {
-      desired_roc = ROC_IN_ERROR;
-    }
-  }
+  double desired_angle_deg = -pos_error * DEGREES_PER_METER;
   
   double output = 0.0;
   
   // just P control for now
-  double roc_error = x_roc - desired_roc;
-  output += ROC_KP * roc_error;
+  double angle_error_deg = angle_deg - desired_angle_deg;
+  output -= ANGLE_KP * angle_error_deg;
+  
+  // TODO: D control
   
   Serial.print("X: ");
   Serial.print(x);
-  Serial.print(" X/t: ");
-  Serial.println(x_roc);
+  Serial.print(" angle: ");
+  Serial.println(angle_deg);
   
   return output;
 }
--- ultra_reader/ultra_reader.ino
+++ ultra_reader/ultra_reader.ino
@@ -4,42 +4,34 @@
 #include "turn_control_state.h"
 
 // The boat's pinout
-#define FRONT_ULTRA_TRIGGER 6
-#define FRONT_ULTRA_ECHO 7
+#define FRONT_ULTRA_TRIGGER 4
+#define FRONT_ULTRA_ECHO 5
 
-#define SIDE_ULTRA_TRIGGER 4
-#define SIDE_ULTRA_ECHO 5
+#define BACK_ULTRA_TRIGGER 6
+#define BACK_ULTRA_ECHO 7
 
-#define PB_PIN 12
+#define SWITCH_PIN 12
 
 // Sensor objects
 struct UltraState front_ultra;
-struct UltraState side_ultra;
+struct UltraState back_ultra;
 
 // actuator objects
 Servo turn_servo;
 Servo speed_controller;
 
 // Physical constants of the boat
-#define STRAIGHT_ANGLE 98
-// #define TURN_ADJUST 7
-#define TURN_ADJUST 0
-
-#define ANGLE_PER_DISTANCE (0.0)
-#define LEFT_MAX 80
-
-#define TURN_SENSITIVITY 0.3
-
-#define MAX_TURN_HARDNESS 15
+#define STRAIGHT_ANGLE 65
+#define MAX_TURN_HARDNESS 23
 
 void setup() {
   Serial.begin(9600);
   make_ultra(&front_ultra, FRONT_ULTRA_TRIGGER, FRONT_ULTRA_ECHO);
-  make_ultra(&side_ultra, SIDE_ULTRA_TRIGGER, SIDE_ULTRA_ECHO);
+  make_ultra(&back_ultra, BACK_ULTRA_TRIGGER, BACK_ULTRA_ECHO);
   turn_servo.attach(3);
   speed_controller.attach(2);
-  pinMode(PB_PIN, INPUT);
-  digitalWrite(PB_PIN, HIGH); // internal pull-up
+  pinMode(SWITCH_PIN, INPUT);
+  digitalWrite(SWITCH_PIN, HIGH); // internal pull-up
 }
 
 void loop() {
@@ -50,13 +42,13 @@ void loop() {
   delay(3000);
   
   Serial.println("Waiting to start...");
-  // while (digitalRead(PB_PIN) == HIGH) {}
+  while (digitalRead(SWITCH_PIN) == HIGH) {}
   Serial.println("starting");
   
   unsigned long cur_time = micros();
   
   // speed_controller.writeMicroseconds(1150);
-  speed_controller.writeMicroseconds(1160);
+  // speed_controller.writeMicroseconds(1160);
   // speed_controller.writeMicroseconds(2000);
   
   delay(1000);
@@ -64,30 +56,27 @@ void loop() {
   TurnControlState turn_control;
   turn_control_init(&turn_control);
 
-  // while (digitalRead(PB_PIN) == HIGH) {
-  while (1) {
-    cur_time = micros();
-    update_ultra(&side_ultra, cur_time);
+  while (digitalRead(SWITCH_PIN) == LOW) {
     cur_time = micros();
     update_ultra(&front_ultra, cur_time);
+    cur_time = micros();
+    update_ultra(&back_ultra, cur_time);
     
     int turn_angle = STRAIGHT_ANGLE; // - ANGLE_PER_DISTANCE*(0.3 - (double)side_ultra.cur_distance);
     
-    if (front_ultra.cur_distance < 0.40) {
-      turn_angle = LEFT_MAX;
-    } else {
-      double control_hardness = turn_control_cycle(&turn_control, side_ultra.cur_distance, side_ultra.cur_rate_of_change, cur_time);
-      turn_angle += max(-MAX_TURN_HARDNESS, min(MAX_TURN_HARDNESS, control_hardness));
-    }
+    double control_hardness = turn_control_cycle(&turn_control, front_ultra.cur_distance, back_ultra.cur_distance, cur_time);
+    turn_angle += max(-MAX_TURN_HARDNESS, min(MAX_TURN_HARDNESS, control_hardness));
     
     turn_servo.write(turn_angle);
     
     //Serial.print("A: ");
     //Serial.print(turn_angle);
-    //Serial.print(" F: ");
-    /*Serial.print(front_ultra.cur_distance);
-    Serial.print(", S: ");
-    Serial.print(side_ultra.cur_distance);*/
+    /*
+    Serial.print(" F: ");
+    Serial.print(front_ultra.cur_distance);
+    Serial.print(", B: ");
+    Serial.println(back_ultra.cur_distance);
+    */
     // Serial.print(" S/t: ");
     // Serial.println(side_ultra.cur_rate_of_change);
     
