84eb047c1b756d3e5c7be527dc6508355b49e157
Mark Shen <consid3r@gmail.com>
2013-03-20 23:47:10 -0400

    First attempt with the new ultrasonic front sensor

--- ultra_reader/sensor.ino
+++ ultra_reader/sensor.ino
@@ -22,6 +22,23 @@ void make_ultra(struct UltraState *ultra, int trigger, int echo) {
   ultra->last_reading_time_us = 0;
 }
 
+void make_head(struct UltraState *ultra, int trigger, int echo) {
+  ultra->trigger_pin = trigger;
+  ultra->echo_pin = echo;
+  uint8_t EnPwmCmd[4]={0x44,0x02,0xbb,0x01};    
+  pinMode(trigger, OUTPUT);
+  digitalWrite(trigger,HIGH);
+  pinMode(echo, INPUT);
+  
+  for(int i=0;i<4;i++){
+    Serial.write(EnPwmCmd[i]);
+  }  
+   
+  ultra->cur_distance = 0;
+  ultra->cur_rate_of_change = 0;
+  ultra->last_reading_time_us = 0;
+}
+
 #define ALPHA 0.2
 
 void update_ultra(struct UltraState *ultra, unsigned long cur_time_us) {
@@ -42,4 +59,26 @@ double readFrontRange(int pin_num) {
   // convert from ADC range to 0V0-5V0 range
   double voltage = (double)(analogRead(pin_num)) * 5.0 / 1023.0;
   return voltage;
+}
+
+void update_head(struct UltraState *ultra, unsigned long cur_time_us) {
+    
+  digitalWrite(ultra->trigger_pin, LOW);
+  digitalWrite(ultra->trigger_pin, HIGH);               // reading Pin PWM will output pulses
+     
+  unsigned long DistanceMeasured=pulseIn(ultra->echo_pin,LOW);
+  double new_distance = 0; 
+  if(DistanceMeasured==50000){              // the reading is invalid.
+    Serial.print("Invalid");    
+  }
+   else{
+    new_distance=(double)DistanceMeasured / 2900.0 / 2.0;           // every 50us low level stands for 1cm
+   }
+  Serial.print("Distance=");
+  Serial.print(new_distance);
+  Serial.println("m");
+ 
+  ultra->cur_rate_of_change = 1000000.0 * (new_distance - ultra->cur_distance) / (double)(cur_time_us - ultra->last_reading_time_us);
+  ultra->cur_distance = new_distance;
+  ultra->last_reading_time_us = cur_time_us;    
 }
--- ultra_reader/ultra_reader.ino
+++ ultra_reader/ultra_reader.ino
@@ -10,13 +10,20 @@
 #define BACK_ULTRA_TRIGGER 6
 #define BACK_ULTRA_ECHO 7
 
+#define HEAD_ULTRA_TRIGGER 10
+#define HEAD_ULTRA_ECHO 11
+
 #define SWITCH_PIN 12
 
 #define IR_PIN A4
 
+#define STRAIGHTS 1600
+#define TURNS    1500
+
 // Sensor objects
 struct UltraState front_ultra;
 struct UltraState back_ultra;
+struct UltraState head_ultra;
 
 // actuator objects
 Servo turn_servo;
@@ -28,6 +35,7 @@ Servo speed_controller;
 
 void setup() {
   Serial.begin(9600);
+  make_head(&head_ultra, HEAD_ULTRA_TRIGGER, HEAD_ULTRA_ECHO);
   make_ultra(&front_ultra, FRONT_ULTRA_TRIGGER, FRONT_ULTRA_ECHO);
   make_ultra(&back_ultra, BACK_ULTRA_TRIGGER, BACK_ULTRA_ECHO);
   turn_servo.attach(3);
@@ -69,27 +77,31 @@ void loop() {
   Serial.println("starting");
   
   unsigned long cur_time = micros();
-  
+ 
   // speed_controller.writeMicroseconds(1150);
-  speed_controller.writeMicroseconds(1700);
+  //speed_controller.writeMicroseconds(1600); //Leigh's Final Tune
+  speed_controller.writeMicroseconds(STRAIGHTS);
   // speed_controller.writeMicroseconds(2000);
-  
+
   delay(1000);
   
   TurnControlState turn_control;
   turn_control_init(&turn_control);
-
   while (digitalRead(SWITCH_PIN) == LOW) {
     cur_time = micros();
     update_ultra(&front_ultra, cur_time);
     cur_time = micros();
     update_ultra(&back_ultra, cur_time);
-    
+    cur_time = micros();
+    update_head(&head_ultra, cur_time);
     int turn_angle = STRAIGHT_ANGLE; // - ANGLE_PER_DISTANCE*(0.3 - (double)side_ultra.cur_distance);
     
-    double ir_voltage = readFrontRange(IR_PIN);
-    if (ir_voltage > 0.38) {
+    //double ir_voltage = readFrontRange(IR_PIN);
+    
+    //if (ir_voltage > 0.38) {
+    if (head_ultra.cur_distance < 0.90){
       turn_angle = STRAIGHT_ANGLE - MAX_TURN_HARDNESS;
+      speed_controller.writeMicroseconds(TURNS);
     } else {
       // find the angle that the controller wants to set the servo to
       double control_hardness = turn_control_cycle(&turn_control, front_ultra.cur_distance, back_ultra.cur_distance, cur_time);
@@ -99,16 +111,13 @@ void loop() {
       turn_angle -= max(-MAX_TURN_HARDNESS, min(MAX_TURN_HARDNESS, control_hardness));
     }
     turn_servo.write(turn_angle);
- 
-    // Serial.print("IR: ");
-    // Serial.println(ir_voltage);
- 
-    
+   
     Serial.print(" F: ");
     Serial.print(front_ultra.cur_distance);
     Serial.print(", B: ");
     Serial.println(back_ultra.cur_distance);
-    
+    Serial.print(", H: ");
+    Serial.println(head_ultra.cur_distance);
     delayMicroseconds(20);
   }
 }
